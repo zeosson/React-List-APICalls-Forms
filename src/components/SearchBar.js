5ad525ec4ca66ecd789fcebf7e508471742c96a8
Mario Gonzalez <zeosson@gmail.com>
2019-07-11 14:57:30 -0500

    Example of controlled and uncontrolled elements.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+state = {term: ''};
+
+  // I wont need this if i use the event handler
+  //   onInputChange(event) {
+  //     console.log(event.target.value);
+  //   }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-              <label>Image Search</label>
-            <input type="text" />
+            <label>Image Search</label>
+            {/* Dont use () when rendering, this is a call back function  */}
+            {/* <input type="text" onChange={this.onInputChange} />  */}
+            <input
+              type="text"
+              // This is an example of a controlled elements
+              value={this.state.term}
+              onChange={event => this.setState({term: event.target.value})}
+            />
           </div>
         </form>
       </div>
