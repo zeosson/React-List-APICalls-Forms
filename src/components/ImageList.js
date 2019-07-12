447c4d5c017a52610067328b46dfa7fd9e526325
Mario Gonzalez <zeosson@gmail.com>
2019-07-12 10:57:12 -0500

    made images scale based on their height

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import './ImageList.css'
 
-import React from "react";
+import React from 'react';
+
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
   const images = props.images.map((image) => {
-    return <img alt={image.description} key={image.id} src={image.urls.regular}/>
+    return <ImageCard key={image.id} image={image}/>
   });
 
   return <div className="image-list">{images}</div>
